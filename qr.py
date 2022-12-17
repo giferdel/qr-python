5e15e55227b5954312c807d0b8c126062f285e2e
GiFerDel <giferdel@gmail.com>
2022-12-17 09:54:59 -0300

    first commit

---
+++ qr.py
@@ -0,0 +1,10 @@
+import qrcode
+
+link = 'https://linktr.ee/giferdel'
+qr = qrcode.QRCode(version=2,box_size=20,border=5)
+qr.add_data(link)
+qr.make(fit=True)
+
+img=qr.make_image(fill='black',back_color='white')
+img.save('qr.png')
+
