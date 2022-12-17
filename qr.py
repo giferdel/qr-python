4130d316c678625c9bd8dfb2498f1e120d4122c3
GiFerDel <giferdel@gmail.com>
2022-12-17 13:39:11 -0300

    cambio color

--- qr.py
+++ qr.py
@@ -5,6 +5,6 @@ qr = qrcode.QRCode(version=2,box_size=20,border=5)
 qr.add_data(link)
 qr.make(fit=True)
 
-img=qr.make_image(fill='black',back_color='white')
+img=qr.make_image(fill='black',back_color='red')
 img.save('qr.png')
 
